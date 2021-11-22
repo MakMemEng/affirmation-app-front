90fa339351a5d145a7134a0919cf34eb35f22849
makmemeng <yatani.mems4568@gmail.com>
2021-11-23 02:58:08 +0900

    Add individual affirmation pages

---
+++ layouts/affirmation.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <logged-in-app-bar />
+    <v-main>
+      affirmation.vue
+      <nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+}
+</script>
